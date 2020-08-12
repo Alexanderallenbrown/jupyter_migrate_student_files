55e389bd945bb9c482f315888f23da7ad8c32b54
Alexander Brown <brownaa@lafayette.edu>
2020-08-12 11:37:45 -0400

    first commit

---
+++ zipper.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+#list of students in NEW course
+students = ['bontjen','cumminmj','fiskez','galbrajw','gasparg','steurerg','hurleyr','keanee','olivasm','wengerb']
+
+#what course?
+course = 'ES103'
+
+#current home directory
+dir_working = os.getcwd()
+
+#directory to hold ALL files before tar
+dir_staging = course+'_tar'
+
+#create that directory
+if not os.path.exists(dir_staging):
+    os.makedirs(dir_staging)
+
+#now set up the directory where all student files live (home)
+dir_homeroot = '/home/'
+
+#change to home directory
+os.chdir(dir_homeroot)
+
+#now walk through the home directory
+for root, dirs, files in os.walk(".", topdown=False):
+    for current_dir in dirs:
+        for student in students:
+            if (student==current_dir and root=='.'):
+                print("Found student directory: "+os.path.join(root,current_dir))
+		shutil.copytree(os.path.join(root,current_dir),os.path.join(dir_working,dir_staging,student,course))
